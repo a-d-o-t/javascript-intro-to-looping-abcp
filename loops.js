96d6ff57eb2f8003cc1cd6174298e1bb1b9ef850
Amanda Bromberg <7b4571c5@opayq.com>
2018-01-21 16:54:40 +0000

    automatically backed up by learn

---
+++ loops.js
@@ -0,0 +1,28 @@
+var newArray = ["three","two","one"]
+
+function forLoop(newArray){
+  for (let i=0; i<25; i++) {
+    if (i===1){
+      newArray.push["I am 1 strange loop."];
+    }
+    else {
+      newArray.push[`I am ${i} strange loops.`];
+    }
+    //return newArray
+  }
+}
+
+function forLoop(array) {
+
+	for (let i=0; i<25; i++) {
+
+		if (i===1) {
+			array.push(“I am 1 strange loop.”);
+				}
+
+		else {
+			array.push(“I am ${i} strange loops.”);
+			  }
+}
+		return array
+}
